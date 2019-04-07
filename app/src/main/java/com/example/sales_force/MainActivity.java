ba6a809c765d2fcf68a94a342203c5f8f71247ea
Bruno Caride Dutra <brunocaride@ucl.br>
2019-04-07 13:14:54 -0300

    Segundo Commit versão 1.1

--- app/src/main/java/com/example/sales_force/MainActivity.java
+++ app/src/main/java/com/example/sales_force/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.sales_force;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+
+    public void OnClickbuttonLogin (View view){
+
+        Intent call_activity_user = new Intent(this, CreateUserActivity.class);
+        startActivity(call_activity_user);
+
+    }
+
 }
+
+
+
+
